6cbdd012e4fcab1fd5ea5a5abe6ee5e01c3a88b2
Darius <corina.dariusnicu@gmail.com>
2026-09-10 12:09:23 +0200

    feat: new files

---
+++ 01-python-foundations/basics.py
@@ -0,0 +1,27 @@
+# Basic/Fundamentals of Python
+
+#Strings
+
+print ("Hello Space") #This is a string
+
+# Arithmetic Operations
+
+print (2 + 296) # Addition
+print (987 - 201) #Subtraction
+print (812 * 333321) #Multiplication
+print (711 / 99102) #Division
+print (2111 ** 3) #Exponentiation
+print (29945 % 3299) #Modulus
+print (2910 // 320) #Floor Division
+
+# Data types and how to check them
+
+print (type(300000)) #Integer
+print (type(300000.01)) #Float
+print (type("Hello Space")) #String
+print (type(13 + 148829j)) #Complex
+print (type(False)) #Boolean
+print (type([20311929, 187973992, 2932020000919199020209])) #List
+print (type({"Name": "Space", "Size": "We think and estimate it is infinite", "Age": "Unknown", "Location": "Everywhere"})) #Dictionary
+print (type({300011, 3391910, 939302022222293930})) #Set
+print (type((18291802912, 1838830292, 9282182901))) #Tuple
