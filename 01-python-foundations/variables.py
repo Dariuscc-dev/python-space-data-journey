e97987a9415b9c7f0f8d924f37a829e4ae3aa4a9
Darius <corina.dariusnicu@gmail.com>
2026-09-20 18:48:24 +0200

    variables

---
+++ 01-python-foundations/variables.py
@@ -0,0 +1,23 @@
+# Variables are named spaces in the computer's memory where we can store data.
+# Mnemonic names are advised.
+
+# Here are some variable-naming rules =
+
+# It is not possible for the name of a variable to begin with = a number, a special character nor a hyphen.
+# Descriptive names for variables are advised.
+
+# Extra Python variable name rules =
+
+# A variable name must start with a letter or the underscore character.
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+# Variable names are case-sensitive.
+
+# It is also advised to use Python's standard variable naming convention (snake_case).
+
+# When we assign certain data type to a variable we call it a declaration.
+# The equal sign is an assignment operator (to store data in a variable).
+
+first_name = "Darius"
+my_planet = "Sea-12239402"
+
+
